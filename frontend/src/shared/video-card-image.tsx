6a5acdaa39492c4b74d86d87b248e8acd0dadae8
Raul <raul.akerman@gmail.com>
2023-02-15 03:20:17 +0200

    In-progress smoothing of loading bar (NOT DONE)

--- frontend/src/shared/video-card-image.tsx
+++ frontend/src/shared/video-card-image.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import{useSpring, animated}from"react-spring";
+
 interface Props {
   videoId: string;
   progress?: number;
@@ -9,6 +12,21 @@ interface Props {
  * Renders the thumbnail of a video or the progress of the video if it is being processed
  */
 export default function VideoCardImage({ progress, videoId }: Props) {
+
+  const [oldVal, setOldVal] = useState<number>(0);
+
+  const barvalue = useSpring({
+    from: { progress: oldVal },
+    config: { duration: 1500 },
+    to: { progress: progress ?? oldVal },
+  });
+
+  useEffect(() => {
+    if (progress) {
+      setOldVal(progress);
+    }
+  }, [progress]);
+
   if (!progress) {
     return <img src={`/api/clips/${videoId}/thumbnail.jpg`} />
   }
@@ -19,6 +37,8 @@ export default function VideoCardImage({ progress, videoId }: Props) {
 
   // @TODO: Since we have a thumbnail here we can maybe add some opacity to it and show the progress on top of it
   return (
-    <div className="radial-progress" style={{"--value":progress} as any}>{`${progress}%`}</div>
+    <animated.div className="radial-progress" style={{"--value":progress} as any}>
+      {barvalue.progress.to((p) => p.toFixed(2) + "%" )}
+      </animated.div>
   )
 }
