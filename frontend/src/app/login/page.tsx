7c8af80b37ea67b041f2861b4ce5c9aae36d2d9f
Alexander Oweka <alexanderolalekanoweka@gmail.com>
2023-02-15 13:38:00 +0100

    bugfix: added enter key support for register/login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
 import { useRouter } from "next/navigation";
 import { login } from "@/shared/api";
 import { UserContext } from "@/context/user-context";
@@ -12,7 +12,8 @@ export default function Home() {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
-  const loginUser = async () => {
+  const loginUser = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     const ok = await login(username, password);
     if (ok) {
       userContext.reload();
@@ -23,36 +24,40 @@ export default function Home() {
   return (
     <main className="h-screen">
       <div className="container mx-auto flex flex-col space-y-6 justify-center items-center py-3">
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
+        <form className="form-control w-full max-w-xs" onSubmit={loginUser} id="loginForm">
+          <label className="label" htmlFor="username">
             <span className="label-text">Username</span>
           </label>
           <input
             type="text"
             placeholder="Username"
+            id="username"
             className="input input-bordered w-full max-w-xs"
             onChange={(e) => {
               setUsername(e.target.value);
             }}
           />
-          <label className="label">
+          <label className="label" htmlFor="password">
             <span className="label-text">Password</span>
           </label>
           <input
             type="password"
             placeholder="Password"
+            id="password"
             className="input input-bordered w-full max-w-xs"
             onChange={(e) => {
               setPassword(e.target.value);
             }}
           />
-        </div>
-        <button className="btn btn-primary w-full max-w-xs" onClick={loginUser}>
+        </form>
+
+        <button className="btn btn-primary w-full max-w-xs" form="loginForm">
           Login
         </button>
 
         <button
           className="btn btn-outline w-full max-w-xs"
+          type="button"
           onClick={() => {
             router.push("/register");
           }}
