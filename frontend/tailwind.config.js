a8a3d8b836c3c4985bd94ee64584fe2406b70933
CursedGamerZ <Deadlydollar@hotmail.com>
2023-02-21 02:11:36 -0600

    broken

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
+    fontFamily: {
+      library: ['LIBRARY 3 AM']
+    }
   },
   daisyui: {
     themes: ["business"],
