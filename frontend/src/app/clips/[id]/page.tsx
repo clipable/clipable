7b59a0a6d396d055d9f09e6c758060f0c4f56819
Chris Bockover <cbock1337@gmail.com>
2023-02-22 21:34:26 -0500

    Add title, desc, views and upload date

--- frontend/src/app/clips/[id]/page.tsx
+++ frontend/src/app/clips/[id]/page.tsx
@@ -1,18 +1,47 @@
 "use client";
 
-import dynamic from 'next/dynamic';
+import { getVideo, Video } from "@/shared/api";
+import { formatDate } from "@/shared/date-formatter";
+import { formatViewsCount } from "@/shared/views-formatter";
+import dynamic from "next/dynamic";
+import { useState, useEffect } from "react";
 
-const ShakaPlayer = dynamic(
-  () => import('shaka-player-react'), 
-  { ssr: false },
-);
+const ShakaPlayer = dynamic(() => import("shaka-player-react"), { ssr: false });
 
 import "shaka-player-react/dist/controls.css";
 
 export default function Page({ params }: { params: { id: string } }) {
+  const [video, setVideo] = useState<Video | null>(null);
+
+  useEffect(() => {
+    const fetchVideo = async () => {
+      const vid = await getVideo(params.id);
+      setVideo(vid);
+    };
+    fetchVideo();
+  }, [params.id]);
+
   return (
-    <main className="bg-white dark:bg-black max-w-[70%] mt-6 mx-auto">
-      <ShakaPlayer src={`/api/clips/${params.id}/dash.mpd`} autoPlay={true} />
+    <main className="max-w-[70%] mt-6 mx-auto">
+      <ShakaPlayer src={`/api/clips/${params.id}/dash.mpd`} autoPlay />
+      {video && (
+        <div className="p-4 flex flex-row">
+          <div>
+            <div>
+              <h1 className="text-2xl font-bold">{video.title}</h1>
+            </div>
+            <p className="text-gray-300">{video.description}</p>
+          </div>
+          <div className="flex-grow"></div>
+          <div className="flex flex-row space-x-2">
+            <p className="text-gray-400 text-xl">{formatDate(video.created_at)}</p>
+            <p className="text-gray-400 text-xl">•</p>
+            <p className="text-gray-400 text-xl">
+              {formatViewsCount(video.views)} view{video.views === 1 ? "" : "s"}
+            </p>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
