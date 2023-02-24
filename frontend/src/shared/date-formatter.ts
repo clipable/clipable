45cd299a3f59ce78f5104d0ccb4625ab9eebda26
CameronLehr <Cameron_lehr@yahoo.com>
2023-02-23 18:12:31 -0600

    Added relative time to the video card

--- frontend/src/shared/date-formatter.ts
+++ frontend/src/shared/date-formatter.ts
@@ -1,7 +1,7 @@
-export const formatDate = (date: Date | string): string => {
-  return new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  }).format(new Date(date));
-};
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
+
+export const formatDate = (date: Date | string) => {
+  return dayjs(date).fromNow()
+}
