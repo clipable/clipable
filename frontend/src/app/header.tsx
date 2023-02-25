57a629a5a54b714fd4b791526c955e364ca18f06
CursedGamerZ <Deadlydollar@hotmail.com>
2023-02-25 17:34:47 -0600

    Formatting

--- frontend/src/app/header.tsx
+++ frontend/src/app/header.tsx
@@ -29,7 +29,15 @@ export default function Header() {
                 <li tabIndex={0}>
                   <a className="uppercase font-semibold">
                     {userContext.user?.username}
-                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                    <svg
+                      className="fill-current"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                    </svg>
                   </a>
                   <ul className="bg-base-100 w-full">
                     {userContext.loggedIn && !userContext.loading && (
