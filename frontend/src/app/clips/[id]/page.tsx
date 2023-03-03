619159d74783282f867a4b18b9dbf684dac74570
Chris <cbock1337@gmail.com>
2023-03-02 23:26:09 -0500

    Add volume to local storage (#124)

--- frontend/src/app/clips/[id]/page.tsx
+++ frontend/src/app/clips/[id]/page.tsx
@@ -7,18 +7,40 @@ import dynamic from "next/dynamic";
 import { useState, useEffect, useContext } from "react";
 import "@mkhuda/react-shaka-player/dist/ui.css";
 import Link from "next/link";
-import "./player.scss"
-import trashcan from "../../../../public/trashcan.svg"
+import "./player.scss";
+import trashcan from "../../../../public/trashcan.svg";
 import { UserContext } from "@/context/user-context";
 import { useRouter } from "next/navigation";
 
-const ReactShakaPlayer = dynamic(() => import("@mkhuda/react-shaka-player").then(module => module.ReactShakaPlayer), { ssr: false });
+const ReactShakaPlayer = dynamic(() => import("@mkhuda/react-shaka-player").then((module) => module.ReactShakaPlayer), {
+  ssr: false,
+});
 
 export default function Page({ params }: { params: { id: string } }) {
   const [videoDetails, setVideoDetails] = useState<Video | null>(null);
   const userContext = useContext(UserContext);
   const router = useRouter();
-  
+  let [mainPlayer, setMainPlayer] = useState({});
+
+  useEffect(() => {
+    const { player, videoElement } = mainPlayer as { player: any; videoElement: HTMLVideoElement };
+    if (!player) return;
+
+    const play = async () => {
+      videoElement.onvolumechange = () => {
+        localStorage.setItem("volume", videoElement.volume.toString());
+      };
+      await player.load(`/api/clips/${params.id}/dash.mpd`);
+      videoElement.volume = parseFloat(localStorage.getItem("volume") || "1");
+      videoElement.play();
+    };
+    play();
+    return () => {
+      player.unload();
+      videoElement.onvolumechange = null;
+    };
+  }, [mainPlayer]);
+
   useEffect(() => {
     const fetchVideo = async () => {
       const vid = await getVideo(params.id);
@@ -30,24 +52,21 @@ export default function Page({ params }: { params: { id: string } }) {
   return (
     <main className={`mt-2`}>
       <div className="w-fit mx-auto">
-        <ReactShakaPlayer
-          src={`/api/clips/${params.id}/dash.mpd`}
-          autoPlay
-        />
+        <ReactShakaPlayer onLoad={(player) => setMainPlayer(player)} autoPlay />
       </div>
       {videoDetails && (
         <div className="p-4 mx-auto flex flex-row container">
           <div className="w-full  overflow-hidden text-ellipsis whitespace-nowrap">
-            <h1 className="text-2xl font-bold w-[90%] max-w-[90%] overflow-hidden whitespace-nowrap text-ellipsis">{videoDetails.title}</h1>
+            <h1 className="text-2xl font-bold w-[90%] max-w-[90%] overflow-hidden whitespace-nowrap text-ellipsis">
+              {videoDetails.title}
+            </h1>
             <p className="text-gray-300">{videoDetails.description}</p>
           </div>
           <div className="flex-grow"></div>
           <div className="flex flex-row space-x-2 items-center text-gray-400 text-xl">
             <p className="flex flex-row">
               <p className="hover:text-gray-300">
-                <Link href={`/users/${videoDetails.creator.id}`}>
-                  {videoDetails.creator.username}
-                </Link>
+                <Link href={`/users/${videoDetails.creator.id}`}>{videoDetails.creator.username}</Link>
               </p>
             </p>
             <p className="text-sm">•</p>
@@ -55,18 +74,20 @@ export default function Page({ params }: { params: { id: string } }) {
               {formatViewsCount(videoDetails.views)} view{videoDetails.views === 1 ? "" : "s"}
             </p>
             <p className="text-sm">•</p>
-            <p className="whitespace-nowrap">
-              {formatDate(videoDetails.created_at)}
-            </p>
+            <p className="whitespace-nowrap">{formatDate(videoDetails.created_at)}</p>
             {videoDetails.creator.id === userContext.user?.id && (
               <>
                 <p className="text-sm">•</p>
-                <img src={trashcan.src} className="w-4 text-gray-400 hover:text-gray-300 cursor-pointer" alt="delete clip" title="Delete Clip" onClick={
-                  async () => {
+                <img
+                  src={trashcan.src}
+                  className="w-4 cursor-pointer"
+                  alt="delete clip"
+                  title="Delete Clip"
+                  onClick={async () => {
                     await deleteVideo(videoDetails.id);
                     router.push("/");
-                  }
-                } />
+                  }}
+                />
               </>
             )}
           </div>
