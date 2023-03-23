f5026a2c08a5f781f48bca0486cf2a9ba5b05310
CameronLehr <Cameron_lehr@yahoo.com>
2023-03-22 23:22:56 -0500

    tweaks

--- frontend/src/app/footer.tsx
+++ frontend/src/app/footer.tsx
@@ -4,14 +4,13 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="footer fixed bottom-0 bg-base-300">
-      <nav className="flex px-2 lg:px-8" aria-label="Bottom">
-        <div>Clipable - A project for self hosting videos</div>
-        <div>View the source code on</div>
-        <Link href="https://github.com/clipable/clipable">
-          <strong>GitHub</strong>
-        </Link>
-      </nav>
+    <footer className="flex fixed bottom-0 items-center place-content-center pb-1 pt-1 w-full bg-base-300 text-gray-400">
+      &copy; 2023 Clipable. All rights reserved.{" "}
+      <a href="https://github.com/clipable/clipable" target="_blank">
+        <div className="text-blue-400"> &nbsp; Open Source on GitHub</div>
+      </a>
+      .
     </footer>
   );
 }
+4;
